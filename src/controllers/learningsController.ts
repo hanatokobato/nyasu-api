0574c504eb41e92a8bc92938bb3f51e12645f2b0
bangld <kobato2096@gmail.com>
2023-11-16 14:41:26 +0700

    Fix response format

--- src/controllers/learningsController.ts
+++ src/controllers/learningsController.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import { Learning } from '../models/learning';
 import moment from 'moment';
-import { Card } from '../models/card';
+import { Card, CardDoc } from '../models/card';
 import { shuffle } from 'lodash';
 
 interface ILearningParams {
@@ -80,11 +80,13 @@ const getReviews = catchAsync(
       next_review_at: {
         $lte: current,
       },
-    }).populate('card');
+    }).populate<{ card: CardDoc }>('card');
 
     res.status(200).json({
-      status: 'success',
-      reviews: shuffle(reviews),
+      success: true,
+      data: {
+        cards: shuffle(reviews).map((r) => r.card),
+      },
     });
   }
 );
